46191b82afcce84ca1d3a02c6ec2c41b0a24e084
BulgakovY <44995950+BulgakovY@users.noreply.github.com>
2018-11-13 12:32:30 +0200

    Update settings.py

--- asterisk_site/settings.py
+++ asterisk_site/settings.py
@@ -143,6 +143,5 @@ EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'bulgakov.ya@gmail.com'
-EMAIL_HOST_PASSWORD = 'A50cSpmh'
 DEFAULT_FROM_EMAIL = 'Bulgakov Yaroslav'
 #DEFAULT_TO_EMAIL = 'Your email'
